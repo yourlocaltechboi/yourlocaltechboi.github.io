4e6d00b5c7bc43ac98ea3d3e8345b117d3a657f1
Anden Wieseler <ttechno@icloud.com>
2022-01-13 19:31:55 +0000

    add pages

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
+const contentWindow = document.querySelector('#contentBlock')
+const navWindow = document.querySelector('#nav')
+
+function hide(item){
+    item.style.display = 'none'
+}
+function show(item){
+    if (document.documentElement.clientWidth <= 700){
+        item.style.display = ''
+    }
+    else{
+        item.style.display = 'flex'
+    }
+}
+
 function setPage(page){
     if (page == 'aboutMe'){
-        
+        if (document.documentElement.clientWidth <= 700){
+            hide(navWindow);
+        }
+        contentWindow.innerHTML = 
+    }
+    if (page == 'github'){
+        window.location.href = 'http://github.yltboi.me'
+    }
+    if (page == 'twitter'){
+        window.location.href = 'http://twitter.yltboi.me'
+    }
+    if (page == 'zedtekFacebook'){
+        window.location.href = 'https://fb.me/ZedTekDevel'
+    }
+    if (page == 'jbWalkthrough'){
+        window.location.href = '/walkthrough'
+    }
+    if (page == 'toggle'){
+        window.location.href = 'https://github.com/ZedTek/toggle-base'
+    }
+    if (page == 'portfolio'){
+
+    }
+    if (page == 'photos'){
+
     }
 }
