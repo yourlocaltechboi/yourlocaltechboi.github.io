93adb2e1cfb31f782ce59d98cfca943a154a024c
Anden Wieseler <ttechno@icloud.com>
2022-01-16 19:02:20 -0600

    remove obsolete files

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ function setPage(page){
     if (page == "aboutMe"){
         setNav(false)
         setBlur(true)
-        mobileContentBlock.innerHTML = '<h1><u>ABOUT ME</u></h1><p>Hi! My name is Anden Wieseler, and I like to make things.</p><p>I make websites and software, and I\'m fluent in HTML, CSS, JavaScript, and Python. I\'m currently also learning Swift.</p><p>I spend my free time outside of work coding, designing, taking photos, playing Minecraft, and listening to my endlessly long Apple Music playlists (my longest is over 700 songs xD).</p><p><b>Currently, I\'m working on the following projects:</b></p><p><b>Jailbreak Walkthrough - </b>This is a web-based walkthrough for jailbreaking iPhones and iPads!</p><p><b>Toggle - </b>Toggle is a modern Discord bot framework built using discord.js, and supports the creation of bots using both code and third-party modules!</p><p><b>Freelance Graphic/Web Design - </b>I love to work on web and graphic design projects in my free time!</p><p style="text-align: center;"><b>Check out my design and photo portfolios as well as my other projects in the navigation pane!</b></p>'
-        contentBlock.innerHTML = '<h1><u>ABOUT ME</u></h1>'
+        contentBlock.innerHTML = '<h1><u>ABOUT ME</u></h1><p>Hi! My name is Anden Wieseler, and I like to make things.</p><p>I make websites and software, and I\'m fluent in HTML, CSS, JavaScript, and Python. I\'m currently also learning Swift.</p><p>I spend my free time outside of work coding, designing, taking photos, playing Minecraft, and listening to my endlessly long Apple Music playlists (my longest is over 700 songs xD).</p><p><b>Currently, I\'m working on the following projects:</b></p><p><b>Jailbreak Walkthrough - </b>This is a web-based walkthrough for jailbreaking iPhones and iPads!</p><p><b>Toggle - </b>Toggle is a modern Discord bot framework built using discord.js, and supports the creation of bots using both code and third-party modules!</p><p><b>Freelance Graphic/Web Design - </b>I love to work on web and graphic design projects in my free time!</p><p style="text-align: center;"><b>Check out my design and photo portfolios as well as my other projects in the navigation pane!</b></p>'
+        mobileContentBlock.innerHTML = contentBlock.innerHTML
     }
     if (page == 'github'){
         setNav(false)
@@ -88,6 +88,7 @@ mobileNavButton.addEventListener("click", () => {
 mobileNameBlock.addEventListener("click", () => {
     mobileContentBlock.innerHTML = '<h1>Anden Wieseler</h1><p style="text-align: center;">(click a link in the navigation pane)</p>'
     setBlur(false)
+    setNav(false)
 })
 
 window.addEventListener("resize", () => {
